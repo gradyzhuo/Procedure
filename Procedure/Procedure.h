dfa2bd9b0e173c51b70b1d0c4771832e18388886
Grady Zhuo <gradyzhuo@gmail.com>
2016-11-22 01:57:27 +0800

    Initial Commit

---
+++ Procedure/Procedure.h
@@ -0,0 +1,19 @@
+//
+//  Procedure.h
+//  Procedure
+//
+//  Created by Grady Zhuo on 22/11/2016.
+//  Copyright © 2016 Grady Zhuo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Procedure.
+FOUNDATION_EXPORT double ProcedureVersionNumber;
+
+//! Project version string for Procedure.
+FOUNDATION_EXPORT const unsigned char ProcedureVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Procedure/PublicHeader.h>
+
+
